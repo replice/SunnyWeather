4858674a867cc54f1da60be1ffbf9bffaf7d3ae8
geliangchen <971804743@qq.com>
2021-10-06 16:34:45 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application(){
+
+
+    companion object{
+        const val TOKEN = "zLtopEfD4wreb3Z3"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
